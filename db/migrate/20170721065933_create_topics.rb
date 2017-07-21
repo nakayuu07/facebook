8916f75072d302e483ca906f53f67fd9b7d21b0a
中村勇哉 <nakamurayuuya@nakamurayuuya-no-MacBook-Air.local>
2017-07-21 17:50:05 +0900

    CRUD

---
+++ db/migrate/20170721065933_create_topics.rb
@@ -0,0 +1,11 @@
+class CreateTopics < ActiveRecord::Migration
+  def change
+    create_table :topics do |t|
+     t.string :title
+     t.text :content
+
+     
+     t.timestamps null: false
+    end
+  end
+end
