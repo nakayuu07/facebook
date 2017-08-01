423448c111bd1547e54eb9c4173603b7412709ce
中村勇哉 <nakamurayuuya@nakamurayuuya-no-MacBook-Air.local>
2017-08-01 18:38:01 +0900

    テストデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::Name.name
+  uid = Faker::Twitter.status
+  provider = Faker::Twitter.status
+  avatar = Faker::Avatar.image
+  image_url = Faker::Avatar.image
+  user = User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: name,
+               uid: uid,
+               provider: provider,
+               avatar: avatar,
+               image_url: image_url
+               )
+   topic = Topic.create(title: "あああ",
+                content: "hoge",
+                user_id: user.id
+               )
+    Comment.create(content: "sss",
+                   user_id: user.id,
+                   topic_id: topic.id
+                   )
+  end
