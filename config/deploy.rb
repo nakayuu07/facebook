628fbe63667b7df570fb681b430cec8977110cbe
中村勇哉 <nakamurayuuya@nakamurayuuya-no-MacBook-Air.local>
2017-08-02 17:41:54 +0900

    change storage to fog from file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'kaobook'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/nakayuu07/facebook'
